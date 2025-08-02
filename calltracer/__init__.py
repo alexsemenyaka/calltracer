50c0c8d0a0801de6468e5e976ef8394acbede5cf
Alex Semenyaka <alex.semenyaka@gmail.com>
2025-08-02 23:11:23 +0200

    Initial commit

--- calltracer/__init__.py
+++ calltracer/__init__.py
@@ -1,10 +1,9 @@
-
-
 """
-calltracer: Some description, maybe?
+calltracer: A debugging module with a decorator (CallTracer) for tracing function calls and a function (stack) for logging the current call stack.
 """
 
 import importlib.metadata
+from .calltracer import CallTracer, stack
 
 _metadata = importlib.metadata.metadata("calltracer")
 __version__ = _metadata["Version"]
@@ -12,4 +11,5 @@ __author__ = _metadata["Author-email"]
 __license__ = _metadata["License"]
 
 __all__ = [
+"CallTracer", "stack"
 ]
